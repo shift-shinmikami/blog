26860c84ad4442262ba78927aede0b5f9f624e2e
Shin_tech25 <m.shin0509@gmail.com>
2022-07-18 18:59:46 +0900

    remove disqus tag

--- Docker/blog-a/src/templates/blog-post/index.js
+++ Docker/blog-a/src/templates/blog-post/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { graphql } from "gatsby"
-import Disqus from "gatsby-plugin-disqus"
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
 import { Tags } from "../../components/modules"
