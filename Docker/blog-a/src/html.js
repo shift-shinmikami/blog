f957b15e454abbba4434e82cfb4a8adbb6c912a7
Shin_tech25 <m.shin0509@gmail.com>
2022-07-22 13:54:09 +0900

    Adsense script tagを追記

--- Docker/blog-a/src/html.js
+++ Docker/blog-a/src/html.js
@@ -11,7 +11,8 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        {/* ここにAdsense Script Tagを追記する */}
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6461117183201003"
+     crossorigin="anonymous"></script>
 
         {props.headComponents}
       </head>
