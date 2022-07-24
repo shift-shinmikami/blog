f2db43d4b260b3f88c2f30105b2ec079e710cbc9
Shin_tech25 <m.shin0509@gmail.com>
2022-07-24 09:49:35 +0900

    modified gatsby-config.js

--- Docker/blog-a/gatsby-config.js
+++ Docker/blog-a/gatsby-config.js
@@ -111,13 +111,7 @@ module.exports = {
               shortname: `shin-tech25`,
             },
           },
-          {
-            resolve: `gatsby-remark-katex`,
-            options: {
-              // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
-              strict: `ignore`
-            }
-          },
+          `gatsby-remark-katex`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
         ],
